6a45903e82537b642710722dd48c52f009ce3ba6
lecture-bisper <bisper@outlook.kr>
2022-04-27 15:27:51 +0900

    부트스트랩을 이용한 웹 화면 만들고 컴포넌트로 분리하기.

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header class="bg-primary text-white text-center p-5">
+      <h1>My First Bootstrap5 Page</h1>
+      <p>반응형 웹 지원됨</p>
+    </header>
+  );
+}
+
+export default Header;
